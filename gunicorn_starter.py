a7ac8ea9a83d9c13a92537f8cf40dd74dc07436b
MoisucSilviu <48621592+MoisucSilviu@users.noreply.github.com>
2025-07-15 13:13:08 +0300

    Update gunicorn_starter.py

--- gunicorn_starter.py
+++ gunicorn_starter.py
@@ -1,11 +1,6 @@
 # gunicorn_starter.py
-
-# Apply the gevent patch first, before any other imports
 from gevent import monkey
 monkey.patch_all()
 
-# Now, import your Flask app instance
+# Ensure this matches your main script's filename
 from app import app
-
-# This file doesn't need a main block,
-# Gunicorn will use the 'app' variable.
